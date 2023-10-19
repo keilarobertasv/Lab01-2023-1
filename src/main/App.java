876c2672d0ad53fae55ae268e890ee5a62717b00
Keila Roberta Da Silva <137004868+keilarobertasv@users.noreply.github.com>
2023-10-19 20:23:21 +0000

    Primeiro commit na aula

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,33 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+
+        for (int i = 0; i<arrayOrdenado.length; i++){
+            if (arrayOrdenado[i] == num){
+                return 1;
+            }
+        
+        }
+
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+       
+        int numero = -100;
+        for(int i = 0; i <arrayDesordenado.length; i++){
+          
+            
+            if (arrayDesordenado[i] > numero){
+                numero = arrayDesordenado[i];
+                
+
+            }
+
+        }
+
+        
+        return numero;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
